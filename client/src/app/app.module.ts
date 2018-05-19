482e81faac29ead37962ca05a6b17a0cbaf77ee8
Innessa Schwartz <innessa.schwartz@gmail.com>
2018-05-18 19:41:59 -0500

    Basic back end - first commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,27 +3,18 @@ import { NgModule }			from '@angular/core';
 import { HttpClientModule }	from '@angular/common/http';
 import { FormsModule }		from '@angular/forms';
 
-import { AppRoutingModule }	from './app-routing.module';
-import { ApiService }		from './api.service';
-
 import { AppComponent }		from './app.component';
-import { HomeComponent }	from './home/home.component';
-import { ExamplesComponent } from './examples/examples.component';
-
 
 @NgModule({
 	declarations: [
-		AppComponent,
-		HomeComponent,
-		ExamplesComponent
+
 	],
 	imports: [
 		BrowserModule,
 		HttpClientModule,
-		FormsModule,
-		AppRoutingModule
+		FormsModule
 	],
-	providers: [ApiService],
+	providers: [],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- server/server.js
+++ server/server.js
@@ -12,39 +12,3 @@ app.listen(config.port, err => {
 });
 
 console.log('Server started.');
-
-
-/**************************************************************************************************
- * Example routes
- *************************************************************************************************/
-
-// GET route to the current server time
-app.get('/api/example/time', (req,res) => {
-	res.json({
-		time: Date.now(),				// Number of milliseconds since Jan 1 1970 (the Unix Epoch)
-		text: Date().toString()			// Date as a string
-	});
-});
-
-// GET route to return the number of seconds the server (this node program) as been up.
-app.get('/api/example/uptime', (req,res) => {
-	res.json({uptime:process.uptime()});
-});
-
-// A simple counter that increases each time the route is triggered
-var count = 0;
-app.get('/api/example/count', (req,res) => {
-	count++;			// Increase the count by 1
-	res.json({
-		count: count	// Return the new count
-	});
-});
-
-// POST request that echoes back the text sent
-app.post('/api/example/echo', (req,res) => {
-	var text = req.body.text;					// req.body is the object POSTed to the route
-	res.json({
-		original: text,
-		caps: text.toUpperCase()
-	});
-});
